af3fd14623a8b5c6615c0b2b18037c543a50557e
rike82 <rike.gruent@googlemail.com>
2024-10-08 20:45:19 +0200

    Deleted useless columns

--- car_center.sql
+++ car_center.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema car_center
 -- -----------------------------------------------------
 
@@ -21,11 +24,13 @@ CREATE TABLE IF NOT EXISTS `car_center`.`stores` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `street` VARCHAR(45) NOT NULL,
-  `postal_code` INT NULL,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  `postal_code` INT NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -34,10 +39,10 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `car_center`.`cars` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `vin` VARCHAR(17) NOT NULL,
-  `manufacturer` VARCHAR(45) NULL,
-  `model` VARCHAR(45) NULL,
-  `year` YEAR(4) NULL,
-  `color` VARCHAR(45) NULL,
+  `manufacturer` VARCHAR(45) NULL DEFAULT NULL,
+  `model` VARCHAR(45) NULL DEFAULT NULL,
+  `year` YEAR NULL DEFAULT NULL,
+  `color` VARCHAR(45) NULL DEFAULT NULL,
   `stores_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `vin_UNIQUE` (`vin` ASC) VISIBLE,
@@ -45,10 +50,10 @@ CREATE TABLE IF NOT EXISTS `car_center`.`cars` (
   INDEX `fk_cars_stores1_idx` (`stores_id` ASC) VISIBLE,
   CONSTRAINT `fk_cars_stores1`
     FOREIGN KEY (`stores_id`)
-    REFERENCES `car_center`.`stores` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `car_center`.`stores` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -57,11 +62,13 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `car_center`.`customers` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `cust_id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  UNIQUE INDEX `cust_id_UNIQUE` (`cust_id` ASC) VISIBLE,
+  `cust_name` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE INDEX `cust_id_UNIQUE` (`cust_id` ASC) VISIBLE,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -70,8 +77,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `car_center`.`salesperson` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `staff_id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `salespersoncol` VARCHAR(45) NULL,
+  `salesperson_name` VARCHAR(45) NOT NULL,
   `stores_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
@@ -79,41 +85,39 @@ CREATE TABLE IF NOT EXISTS `car_center`.`salesperson` (
   INDEX `fk_salesperson_stores_idx` (`stores_id` ASC) VISIBLE,
   CONSTRAINT `fk_salesperson_stores`
     FOREIGN KEY (`stores_id`)
-    REFERENCES `car_center`.`stores` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `car_center`.`stores` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `car_center`.`invoices`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `car_center`.`invoices` (
-  `number` INT NULL,
-  `date` DATE NULL,
-  `invoicescol` VARCHAR(45) NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `inv_number` INT NULL DEFAULT NULL,
+  `inv_date` DATE NULL DEFAULT NULL,
   `salesperson_id` INT NOT NULL,
   `customers_id` INT NOT NULL,
   `cars_id` INT NOT NULL,
   INDEX `fk_invoices_salesperson1_idx` (`salesperson_id` ASC) VISIBLE,
   INDEX `fk_invoices_customers1_idx` (`customers_id` ASC) VISIBLE,
   INDEX `fk_invoices_cars1_idx` (`cars_id` ASC) VISIBLE,
-  CONSTRAINT `fk_invoices_salesperson1`
-    FOREIGN KEY (`salesperson_id`)
-    REFERENCES `car_center`.`salesperson` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invoices_customers1`
-    FOREIGN KEY (`customers_id`)
-    REFERENCES `car_center`.`customers` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   CONSTRAINT `fk_invoices_cars1`
     FOREIGN KEY (`cars_id`)
-    REFERENCES `car_center`.`cars` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `car_center`.`cars` (`id`),
+  CONSTRAINT `fk_invoices_customers1`
+    FOREIGN KEY (`customers_id`)
+    REFERENCES `car_center`.`customers` (`id`),
+  CONSTRAINT `fk_invoices_salesperson1`
+    FOREIGN KEY (`salesperson_id`)
+    REFERENCES `car_center`.`salesperson` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
